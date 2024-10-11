7dfb1c2e9aa6f5823dde527f0719ce34816027b8
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 20:06:33 +0300

    Task 5: Fixing pycodestyle Error

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 ''' Module 5-sum_list.py '''
 from typing import List
 
+
 def sum_list(input_list: list[float]) -> float:
     ''' Finds the sum of the elements in the list '''
     return float(sum(input_list))
