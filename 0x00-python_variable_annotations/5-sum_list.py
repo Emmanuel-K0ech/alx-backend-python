36e037284b792ef53863184b23e840c8f3042fca
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 19:59:17 +0300

    Task 5: Function documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,6 +3,7 @@
 
 
 def sum_list(input_list: list[float]) -> float:
+    ''' Finds the sum of the elements in the list '''
     total: float = 0.0
     for element in input_list:
         total += element
