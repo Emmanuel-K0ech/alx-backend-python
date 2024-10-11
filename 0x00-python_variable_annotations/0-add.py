66239ea19b0ce19bbb44d1b7c1d61302c3121f6d
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 17:37:31 +0300

    Wrapping documentation with single quotes rather than double

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-"""
+'''
 This is a Type-annotated function `add` that takes two variables a & b(float)
 and returns their sum(float)
-"""
+'''
 
 
 def add(a: float, b: float) -> float:
-    """ 
-    Function returns the sum of a & b
-    Args:
-        a(float)
-        b(float)
-    Return: a + b -> float
-    """
+    ''' 
+    Return sum of a & b
+    '''
     return float(a + b)
