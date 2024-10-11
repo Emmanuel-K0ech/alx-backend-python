eec9b887fe2f5eb347f2d37c2ec9fc9a349247dc
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 17:43:34 +0300

    Task 0: Debugging check 3

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-'''
-This is a Type-annotated function `add` that takes two variables a & b(float)
-and returns their sum(float)
+# '''
+# This is a Type-annotated function `add` that takes two variables a & b(float)
+# and returns their sum(float)
+# '''
+
+
+# def add(a: float, b: float) -> float:
+#     ''' 
+#     Add two floating point numbers : a and b
+#     '''
+#     return float(a + b)
+'''Modules of task-0.
 '''
 
 
 def add(a: float, b: float) -> float:
-    ''' 
-    Return sum of a & b
+    '''Adds two floating-point numbers.
     '''
-    return float(a + b)
+    return a + b
