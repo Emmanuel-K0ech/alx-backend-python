9283934f26a30279472d023a16ba526887f87f83
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 18:10:41 +0300

    Task 1: Concatenate strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+ Module for 1-concat.py
+ '''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings and returns result
+    '''
+    return str1 + str2
