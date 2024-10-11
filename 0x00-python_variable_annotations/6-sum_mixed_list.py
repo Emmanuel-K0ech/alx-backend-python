d97e16be759e69ddd7b892fe582bcf0741658b3f
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 20:43:50 +0300

    Task 6: Solving import error from typing

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     ''' Mixed list of int and floats and returns their sum in float'''
     return float(sum(mxd_lst))
