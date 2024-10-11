9557ee8e41dd4589a8c0cfdb805c6e47570c21c1
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 20:42:06 +0300

    Task 7: Sorting import error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ Module 7-to_kv.py - to_kv()
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     ''' Takes a string and number (int or float) and returns a tuple.
         The tuple contains the string and the square of the number as a float.
     '''
