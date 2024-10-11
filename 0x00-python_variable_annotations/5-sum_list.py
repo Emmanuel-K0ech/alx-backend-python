3711c5dbf45f00b6f6facfbd4e9a159c18289b79
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 20:02:16 +0300

    Task 5: using List import methods to do sum

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 ''' Module 5-sum_list.py '''
-
+from typing import List
 
 def sum_list(input_list: list[float]) -> float:
     ''' Finds the sum of the elements in the list '''
-    total: float = 0.0
-    for element in input_list:
-        total += element
-    return total
+    return float(sum(input_list))
