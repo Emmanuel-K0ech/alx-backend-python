c0e047a109aaec551c7e9fcdf4ff55da0bd30943
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 17:33:31 +0300

    Task 0: Updating function documentation for add()

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,4 +6,11 @@ and returns their sum(float)
 
 
 def add(a: float, b: float) -> float:
+    """ 
+    Function returns the sum of a & b
+    Args:
+        a(float)
+        b(float)
+    Return: a + b -> float
+    """
     return a + b
