b0ade594a3b0c8163efa8eadaa4220f1aa226aa5
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-11 17:26:32 +0300

    Task 0: Creating  Type-annotated Function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This is a Type-annotated function `add` that takes two variables a & b(float)
+and returns their sum(float)
+"""
+
+
+def add(a: float, b: float) -> float:
+    return a + b
