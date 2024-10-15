5e4b02a6a787f57e430e56321916552634f72625
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-15 17:09:08 +0300

    Task 4: Import Error correction

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,10 +4,10 @@ import asyncio
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_random(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait_n spawns wait_random n times with the specified max_delay
     Args:
