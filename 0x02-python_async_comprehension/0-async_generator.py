75926e0d40e7409b8f00a6a2ca3060a1826bc69c
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-17 01:17:57 +0300

    Task 0 & 1: Type Annotation for generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Module 0.async_generator.py """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Loops 10 times, each time asynchrounously wait 1 second
     then yield a random module
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """ Module 2-async_comprehension.py """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Returns random numbers receive from an asynchrounous generator
     """
-    result = []
+    result: List[float] = []
     async for res in aiter(async_generator()):
         result.append(res)
     return result
