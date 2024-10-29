7dadd26c1fd006e02a9d0b5e1ac1fa476bfb50c8
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-29 21:40:19 +0300

    Task 1: Code refactor - returning the list directly with a list comprehension code inside to generate the numbers

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 """ Module 2-async_comprehension.py """
-import asyncio
 from typing import List
+from importlib import import_module as using
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
     Returns random numbers receive from an asynchrounous generator
     """
-    result = []
-    async for res in aiter(async_generator()):
-        result.append(res)
-    return result
+    return [numb async for numb in async_generator()]
