79d897d3f43b477ef54f9aef01db6c7b9f0fb259
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-11-04 19:11:27 +0300

    Task 2: Mock HTTP calls - using patch()

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Module test_utils.py """
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch
 import unittest
 
 
@@ -26,7 +27,31 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected):
         """
         Tests whether KeyError is raise
-        context manager -> with
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    TestGetJson inherits from unnittest.TestCase - created to test
+    that utils.get_json returns the expected result
+    """
+    @parameterized.expand([
+        ("https://example.com", {"payload": True}),
+        ("https://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Tests that the utils.get_json returns expected results
+        get method is called exaclty once (per input)
+        Tests that the output get_json === test_payload
+
+        Args:
+            mock_get: clone of the response from request.get code
+        """
+        mock_get.return_value.json.return_value = {"key": "value"}
+        test_payload = get_json(test_url)
+        self.assertEqual(test_payload, {"key": "value"})
+        self.assertIs(mock_get.call_count, 1)
